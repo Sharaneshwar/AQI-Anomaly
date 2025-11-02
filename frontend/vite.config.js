053d233290e43ecb6c275cd9aa6a30d8c9d33bb8
Sharaneshwar <punjalsharaneshwar@gmail.com>
2025-11-02 15:14:40 +0530

    frontend vercel fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const backendUrl = process.env.BACKEND_URL || 'http://localhost:8000'
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: `${backendUrl}`,
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // Load environment variables from .env file
+  const env = loadEnv(mode, process.cwd(), '')
+
+  // Use your backend URL or fallback to localhost
+  const backendUrl = env.VITE_BACKEND_URL || 'http://localhost:8000'
+  console.log('Backend URL:', backendUrl)
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: `${backendUrl}`,
+          changeOrigin: true,
+        },
+        '/cities': {
+          target: `${backendUrl}/api`,
+          changeOrigin: true,
+        },
+        '/sites': {
+          target: `${backendUrl}/api`,
+          changeOrigin: true,
+        },
       },
-      '/cities': {
-        target: `${backendUrl}/api`,
-        changeOrigin: true,
-      },
-      '/sites': {
-        target: `${backendUrl}/api`,
-        changeOrigin: true,
-      }
-    }
+    },
   }
 })
