2a151515dbc62a53a5f8fd1f000198674ed5acaf
Sharaneshwar <punjalsharaneshwar@gmail.com>
2025-11-02 14:51:00 +0530

    updated frontend dynamic backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const backendUrl = process.env.BACKEND_URL || 'http://localhost:8000'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: `${backendUrl}`,
         changeOrigin: true,
       },
       '/cities': {
-        target: 'http://localhost:8000/api',
+        target: `${backendUrl}/api`,
         changeOrigin: true,
       },
       '/sites': {
-        target: 'http://localhost:8000/api',
+        target: `${backendUrl}/api`,
         changeOrigin: true,
       }
     }
